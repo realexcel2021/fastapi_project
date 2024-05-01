b9cc7cda877a98d6303aa647ba85cc014fc5505e
realexcel2021 <realexcel2020@gmail.com>
2024-05-01 19:41:43 +0100

    update sonar

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -4,6 +4,7 @@ data "aws_caller_identity" "data" {
 resource "aws_ecr_repository" "fast_repo" {
   name                 = "fast-api-repo"
   image_tag_mutability = "MUTABLE"
+  force_delete = true
 
   image_scanning_configuration {
     scan_on_push = true
